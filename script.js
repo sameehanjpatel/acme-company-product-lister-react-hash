36138db6cdd97dd624cb5f1803e879a134d10d3a
Sameehan Patel <sp4315@nyu.edu>
2019-08-06 20:50:11 -0400

    setup

---
+++ script.js
@@ -0,0 +1,27 @@
+const {Component} = React;
+const {render} = ReactDOM;
+
+const root = document.querySelector("#root")
+
+class App extends Component{
+    constructor(){
+        super();
+        this.state = {
+            companies: [], 
+            view: "butt"
+        }
+    }
+    componentDidMount(){
+        axio
+    }
+
+    render(){
+      return React.createElement('hr') 
+
+    }
+
+
+
+}
+
+render(React.createElement(App), root)
